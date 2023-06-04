9b521de903c2de2b9ef3159e1990939f4cb0ecb9
Elias Yoc <eliyoc_97@hotmail.com>
2023-06-04 13:01:11 -0500

    fix: mejoras al seleccionar draggableText

--- src/components/Text/styles.js
+++ src/components/Text/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { scaleShowUp } from "../../utils/styledComponents";
 
 export const DraggableTextElement = styled.div`
   max-width: 315px;
@@ -6,8 +7,10 @@ export const DraggableTextElement = styled.div`
   top: 20px;
   position: absolute;
   color: rgba(0, 0, 0, 1);
-  z-index: 15;
+  z-index: ${({ zIndex }) => zIndex || "15"};
   word-break: break-word;
   font-size: 1.4rem;
   padding: 0.5rem 0;
+
+  animation: ${scaleShowUp} 0.3s ease;
 `;
